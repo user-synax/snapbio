c7f424f0f441638d978fc956e73a00b690bd0ddc
user-synax <usersynax@gmail.com>
2026-06-03 08:11:29 +0530

    Add models/Subscription.js

---
+++ models/Subscription.js
@@ -0,0 +1,42 @@
+
+import mongoose from "mongoose";
+
+const SubscriptionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    razorpayOrderId: {
+      type: String,
+    },
+    razorpayPaymentId: {
+      type: String,
+    },
+    plan: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "expired", "pending"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Subscription = mongoose.models.Subscription || mongoose.model("Subscription", SubscriptionSchema);
+
+export default Subscription;
