978430ada727e53a7e2a6da03d71a075662a0730
user-synax <usersynax@gmail.com>
2026-06-03 08:11:28 +0530

    Add models/Link.js

---
+++ models/Link.js
@@ -0,0 +1,38 @@
+
+import mongoose from "mongoose";
+
+const LinkSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+    },
+    order: {
+      type: Number,
+      default: 0,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Link = mongoose.models.Link || mongoose.model("Link", LinkSchema);
+
+export default Link;
