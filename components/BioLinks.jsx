bf58128b7b344637498387c009d29048b2bb9d8f
user-synax <usersynax@gmail.com>
2026-06-12 18:36:07 +0530

    bio links syling

--- components/BioLinks.jsx
+++ components/BioLinks.jsx
@@ -1,45 +1,44 @@
-
 "use client";
 
 import ThemeButton from "./ThemeButton";
 import * as faIcons from "react-icons/fa";
 
-export default function BioLinks({
-  links,
-  theme,
-  userId
-}) {
-  const handleLinkClick = (linkId) => {
-    // Fire and forget
-    fetch("/api/clicks", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        linkId,
-        userId,
-      }),
-    });
-  };
+export default function BioLinks({ links, theme, userId }) {
+    const handleLinkClick = (linkId) => {
+        // Fire and forget
+        fetch("/api/clicks", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                linkId,
+                userId,
+            }),
+        });
+    };
 
-  return (
-    <div className="space-y-4 mt-8">
-      {links.map((link) => {
-        const IconComponent = link.icon ? faIcons[`Fa${link.icon}`] : null;
-        return (
-          <ThemeButton
-            key={link._id}
-            href={link.url}
-            buttonBg={theme.buttonBg}
-            buttonHoverBg={theme.buttonHoverBg}
-            buttonText={theme.buttonText}
-            buttonHoverShadow={theme.buttonHoverShadow}
-            onClick={() => handleLinkClick(link._id)}
-            icon={IconComponent}
-          >
-            {link.title}
-          </ThemeButton>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className="space-y-4 mt-8">
+            {links.map((link) => {
+                const IconComponent = link.icon
+                    ? faIcons[`Fa${link.icon}`]
+                    : null;
+                return (
+                    <ThemeButton
+                        key={link._id}
+                        href={link.url}
+                        buttonBg={theme.buttonBg}
+                        buttonHoverBg={theme.buttonHoverBg}
+                        buttonText={theme.buttonText}
+                        buttonHoverShadow={theme.buttonHoverShadow}
+                        shadowColor={theme.shadowColor}
+                        focusRing={theme.focusRing}
+                        onClick={() => handleLinkClick(link._id)}
+                        icon={IconComponent}
+                    >
+                        {link.title}
+                    </ThemeButton>
+                );
+            })}
+        </div>
+    );
 }
